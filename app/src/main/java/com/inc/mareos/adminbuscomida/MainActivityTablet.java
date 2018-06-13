91f78cde0632c3e3c767ca0a64f550ff76d59e40
MARCOS PEDRAZA <marcos@vladicorreo.com>
2018-06-13 11:57:52 -0500

    Initial commit

---
+++ app/src/main/java/com/inc/mareos/adminbuscomida/MainActivityTablet.java
@@ -0,0 +1,18 @@
+package com.inc.mareos.adminbuscomida;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.inc.mareos.adminbuscomida.R;
+
+public class MainActivityTablet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_tablet);
+
+
+    }
+}
